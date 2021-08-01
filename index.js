1ee0c6686b8fee745b39305813e98d94e43474fd
Beargirl <cool.beargirl@yandex.ru>
2021-08-01 16:04:34 +0300

    changes

--- index.js
+++ index.js
@@ -11,20 +11,25 @@ function check() {
     ("confirm");
 
     document.getElementById('errorMessage').innerHTML = '';
+    document.getElementById('successMessage').innerHTML = '';
+
 
         if(email.value == '')
         document.getElementById('errorMessage').innerHTML+= "Ваш e-mail не заполнен! <br>";
         if(name.value == '')
-        document.getElementById('errorMessage').innerHTML+= "Ваше поле с именем не заполнено! <br>"
+        document.getElementById('errorMessage').innerHTML+= "Поле Имя не заполнено! <br>"
         if(username.value == '')
         document.getElementById('errorMessage').innerHTML+= "Имя пользователя не заполнено! <br>"; 
         if(password.value == '')
         document.getElementById('errorMessage').innerHTML+= "Ваш пароль не заполнен! <br>"; 
         if(confirm.value == '')
         document.getElementById('errorMessage') .innerHTML+= "Повтор пароля не заполнен! <br>"; 
-        if (confirm.value !== 'password')
+        if (confirm.value !== password.value)
         document.getElementById('errorMessage').innerHTML+= "Пароли не совпадают! <br>"; 
-        else 
-        document.getElementsByClassName ('successMessage').innerHTML+= "Добро пожаловать", + 'username' + "!";
+        else {
+
+            document.getElementById ('successMessage').innerHTML+= "Добро пожаловать, <br>" + document.getElementById('name').value + "!";  
+        }
+        
 
 }
