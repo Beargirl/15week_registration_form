5cc5bca9dc2c4f7b4e3e1324f145c4530582e87c
Beargirl <cool.beargirl@yandex.ru>
2021-09-05 22:10:03 +0300

    -

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ function check() {
     document.getElementById('errorMessage').innerHTML = '';
     document.getElementById('successMessage').innerHTML = '';
 
+    let isValid = true; 
 
-        if(email.value == '')
+        if(email.value === '')
         document.getElementById('errorMessage').innerHTML+= "Ваш e-mail не заполнен! <br>";
-        if(name.value == '')
+
+    let isVAlid = 
+        if(name.value === '')
         document.getElementById('errorMessage').innerHTML+= "Поле Имя не заполнено! <br>"
-        if(username.value == '')
+        if(username.value === '')
         document.getElementById('errorMessage').innerHTML+= "Имя пользователя не заполнено! <br>"; 
-        if(password.value == '')
+        if(password.value === '')
         document.getElementById('errorMessage').innerHTML+= "Ваш пароль не заполнен! <br>"; 
-        if(confirm.value == '')
+        if(confirm.value === '')
         document.getElementById('errorMessage') .innerHTML+= "Повтор пароля не заполнен! <br>"; 
         if (confirm.value !== password.value)
         document.getElementById('errorMessage').innerHTML+= "Пароли не совпадают! <br>"; 
@@ -32,4 +35,40 @@ function check() {
         }
         
 
-}
+} 
+
+
+/* function check() {
+    let name = document.getElementById
+    ("name")
+    let email = document.getElementById
+    ("email")
+    let username = document.getElementById
+    ("username")
+    let password = document.getElementById
+    ("password")
+    let confirm = document.getElementById
+    ("confirm");
+
+    element.classList('errorMessage').innerHTML = '';
+    element.classList('successMessage').innerHTML = '';
+
+ 
+        if(email.value === '')
+        element.classList('errorMessage').innerHTML+= "Ваш e-mail не заполнен! <br>";
+        if(name.value === '')
+        element.classList('errorMessage').innerHTML+= "Поле Имя не заполнено! <br>"
+        if(username.value === '')
+        element.classList('errorMessage').innerHTML+= "Имя пользователя не заполнено! <br>"; 
+        if(password.value === '')
+        element.classList('errorMessage').innerHTML+= "Ваш пароль не заполнен! <br>"; 
+        if(confirm.value === '')
+        element.classList('errorMessage') .innerHTML+= "Повтор пароля не заполнен! <br>"; 
+        if (confirm.value !== password.value)
+        element.classList('errorMessage').innerHTML+= "Пароли не совпадают! <br>"; 
+        else {
+            element.classList('successMessage').innerHTML+= "Добро пожаловать, <br>" + document.getElementById('name').value + "!";  
+        }
+        
+
+} */
